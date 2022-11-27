cce723c75c0843a7e8af548546fe23d785b7a2ed
Joshua Hyde <jrh3k5@gmail.com>
2022-11-27 10:17:55 -0600

    Make clickable list reusable

---
+++ ui/component/clickable_list.go
@@ -0,0 +1,97 @@
+package component
+
+import (
+	"strings"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+// ClickableList is a reusable component that shows a list of clickable items
+type ClickableList[V any] struct {
+	listContainer *fyne.Container
+	dataList      *widget.List
+	listFilter    *widget.Entry
+	nameGetter    func(V) string
+	allData       []V
+	currentData   []V
+}
+
+// NewClickableList creates a new instance of ClickableList
+func NewClickableList[V any](nameGetter func(V) string, clickHandler func(V)) *ClickableList[V] {
+	clickableList := &ClickableList[V]{
+		nameGetter: nameGetter,
+	}
+
+	dataList := widget.NewList(func() int {
+		numData := len(clickableList.currentData)
+		if numData < 10 {
+			return 10
+		}
+		return numData
+	}, func() fyne.CanvasObject {
+		button := widget.NewButton("", func() {})
+		button.Alignment = widget.ButtonAlignLeading
+		button.Disable()
+		return button
+	}, func(i widget.ListItemID, o fyne.CanvasObject) {
+		button := o.(*widget.Button)
+		// The list is empty and this just a templated button to help initially fill out the list
+		if i >= len(clickableList.currentData) {
+			button.SetText("")
+			button.Disable()
+			return
+		}
+		datum := clickableList.currentData[i]
+		button.SetText(nameGetter(datum))
+		button.OnTapped = func() {
+			clickHandler(datum)
+		}
+		button.Enable()
+	})
+
+	listFilter := widget.NewEntry()
+	listFilter.Disable()
+	listFilter.SetPlaceHolder("Filter")
+	listFilter.OnChanged = func(v string) {
+		clickableList.applyFilter(v)
+		clickableList.listFilter.Refresh()
+	}
+
+	clickableList.listContainer = container.NewBorder(listFilter, nil, nil, nil, container.NewMax(dataList))
+	clickableList.dataList = dataList
+	clickableList.listFilter = listFilter
+
+	return clickableList
+}
+
+// ClearData clears all clickable data from the list
+func (c *ClickableList[V]) ClearData() {
+	c.allData = nil
+	c.currentData = nil
+	c.dataList.Refresh()
+}
+
+func (c *ClickableList[V]) GetObject() fyne.CanvasObject {
+	return c.listContainer
+}
+
+// SetData sets the data to be displayed within the list
+func (c *ClickableList[V]) SetData(data []V) {
+	c.allData = data
+	c.applyFilter(c.listFilter.Text)
+	c.dataList.Refresh()
+
+	c.listFilter.Enable()
+}
+
+func (c *ClickableList[V]) applyFilter(textFilter string) {
+	var currentData []V
+	for _, datum := range c.allData {
+		if strings.Contains(strings.ToLower(c.nameGetter(datum)), strings.ToLower(textFilter)) {
+			currentData = append(currentData, datum)
+		}
+	}
+	c.currentData = currentData
+}
--- ui/media/genre_list.go
+++ ui/media/genre_list.go
@@ -4,81 +4,39 @@ import (
 	"context"
 	"fmt"
 	"sort"
-	"strings"
 
 	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
 	"github.com/jrh3k5/moo4plex/model"
+	"github.com/jrh3k5/moo4plex/ui/component"
 	"github.com/jrh3k5/moo4plex/ui/services"
 )
 
 type GenreList struct {
-	serviceContainer  *services.ServiceContainer
-	selectorContainer *fyne.Container
-	genresList        *widget.List
-	genreFilter       *widget.Entry
-	allGenres         []*model.Genre
-	currentGenres     []*model.Genre
+	serviceContainer *services.ServiceContainer
+	clickableList    *component.ClickableList[*model.Genre]
 }
 
 func NewGenreList(serviceContainer *services.ServiceContainer, onSelected func(*model.Genre)) *GenreList {
-	component := &GenreList{
+	genreList := &GenreList{
 		serviceContainer: serviceContainer,
 	}
 
-	genreList := widget.NewList(func() int {
-		numGenres := len(component.currentGenres)
-		if numGenres < 10 {
-			return 10
-		}
-		return numGenres
-	}, func() fyne.CanvasObject {
-		button := widget.NewButton("", func() {})
-		button.Alignment = widget.ButtonAlignLeading
-		button.Disable()
-		return button
-	}, func(i widget.ListItemID, o fyne.CanvasObject) {
-		button := o.(*widget.Button)
-		// The list is empty and this just a templated button to help initially fill out the list
-		if i >= len(component.currentGenres) {
-			button.SetText("")
-			button.Disable()
-			return
-		}
-		genre := component.currentGenres[i]
-		button.SetText(genre.Name)
-		button.OnTapped = func() {
-			onSelected(genre)
-		}
-		button.Enable()
+	clickableList := component.NewClickableList[*model.Genre](func(g *model.Genre) string {
+		return g.Name
+	}, func(g *model.Genre) {
+		onSelected(g)
 	})
-	// genreList.Resize(fyne.NewSize(float32(width), float32(height)))
-
-	genreFilter := widget.NewEntry()
-	genreFilter.Disable()
-	genreFilter.SetPlaceHolder("Filter genres")
-	genreFilter.OnChanged = func(v string) {
-		component.applyFilter(v)
-		component.genresList.Refresh()
-	}
-
-	component.selectorContainer = container.NewBorder(genreFilter, nil, nil, nil, container.NewMax(genreList))
-	component.genreFilter = genreFilter
-	component.genresList = genreList
-
-	return component
+	genreList.clickableList = clickableList
+	return genreList
 }
 
 // ClearGenres removes all genres from being selectable
 func (g *GenreList) ClearGenres() {
-	g.allGenres = nil
-	g.currentGenres = nil
-	g.genresList.Refresh()
+	g.clickableList.ClearData()
 }
 
 func (g *GenreList) GetObject() fyne.CanvasObject {
-	return g.selectorContainer
+	return g.clickableList.GetObject()
 }
 
 // SetGenres sets the genres to be shown
@@ -90,20 +48,6 @@ func (g *GenreList) SetGenres(ctx context.Context, mediaLibraryID int64) error {
 	sort.Slice(genres, func(i, j int) bool {
 		return genres[i].Name < genres[j].Name
 	})
-	g.allGenres = genres
-	g.applyFilter(g.genreFilter.Text)
-	g.genresList.Refresh()
-
-	g.genreFilter.Enable()
+	g.clickableList.SetData(genres)
 	return nil
-}
-
-func (g *GenreList) applyFilter(textFilter string) {
-	var currentGenres []*model.Genre
-	for _, genre := range g.allGenres {
-		if strings.Contains(strings.ToLower(genre.Name), strings.ToLower(textFilter)) {
-			currentGenres = append(currentGenres, genre)
-		}
-	}
-	g.currentGenres = currentGenres
 }
